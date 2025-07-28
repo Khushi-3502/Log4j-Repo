6e526ca13d2a2246c5ff3e068113b72ddbe2d924
Khushi Shah <khushishah3502@gmail.com>
2025-07-28 19:30:49 +0530

    changes

---
+++ Log4j/src/test/java/DemoTest/Log4j/Khushi.java
@@ -0,0 +1,14 @@
+package DemoTest.Log4j;
+
+public class Khushi {
+
+	public static void main(String[] args) {
+	
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hi");
+		System.out.println("Hi");
+
+	}
+
+}
