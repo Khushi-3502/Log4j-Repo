51971338ab25de942f9fc6237ccd4eff55ee49af
Khushi Shah <khushishah3502@gmail.com>
2025-07-28 11:15:27 +0530

    New class is added

---
+++ Log4j/src/test/java/DemoTest/Log4j/Testing.java
@@ -0,0 +1,12 @@
+package DemoTest.Log4j;
+
+public class Testing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Creating this for testing purpose");
+
+	}
+
+}
